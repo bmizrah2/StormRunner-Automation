8507efec0368458e0d0b9c772788a8cf51b9ef8e
Benny Mizrachi <bmizrah2@gmail.com>
2019-05-25 15:04:37 +0300

    Fixing case 04 - 'Add monitor' and write all steps in case 04 documentation

--- StormRunner_Automation/src/main/java/com/stormrunner/auto/infra/pages/StormRunnerAssetsMonitorsPage.java
+++ StormRunner_Automation/src/main/java/com/stormrunner/auto/infra/pages/StormRunnerAssetsMonitorsPage.java
@@ -12,7 +12,7 @@ public class StormRunnerAssetsMonitorsPage extends AbstractPage {
 
 	// constructor for AssetsMonitors page
 	public StormRunnerAssetsMonitorsPage(WebDriver driver) throws Exception {
-		super(driver,uploadScriptButton);
+		super(driver,createMonitorButton);
 	}
 
 	//private static final By2 addedToCartTitle = new By2("'Added to cart' title", By.xpath("//h1[contains(text(),'Added to Cart')]"));
@@ -37,10 +37,10 @@ public class StormRunnerAssetsMonitorsPage extends AbstractPage {
 	private static final By2 sitescopeUserNameField = new By2("Entering User Name to connect SiteScope Monitor", By.cssSelector("input.stm-tst-textbox-user-name"));
 
 	// Clicking "Save" button to create SiteScope monitor
-	private static final By2 saveNewMonitorButton = new By2("Clicking 'Save' button in monitor dialog", By.cssSelector("button.stm-tst-save-btn"));
+	private static final By2 saveNewMonitorButton = new By2("'Save' button in monitor dialog", By.cssSelector("button.stm-tst-save-btn"));
 
 	// Clicking "Delete" in main menu for deleting the monitor
-	private static final By2 deleteMonitorButton = new By2("Choosing 'Delete' in Main Menu for deleting the Monitor", By.cssSelector("div.stm-tst-delete"));
+	private static final By2 deleteMonitorButton = new By2("'Delete' in Main Menu for deleting the Monitor", By.cssSelector("div.stm-tst-delete"));
 
 	// For replacing all " " & "." to "-" in the class name
 	private static String monitorName = MainConfig.StormRunnerSitescopeServerName.replace(".", "-").replace(" ", "-");
@@ -51,11 +51,11 @@ public class StormRunnerAssetsMonitorsPage extends AbstractPage {
 	//private static final By2 newTestLine = new By2("'Test created in grid'",                                            
 
 	// Preesing 'yes' in confirmation dialog for deleting the monitor
-	private static final By2 yesButtonInConfirmMonitorDeletion = new By2("Clicking 'Yes' button in confirmation dialog for deleting the monitor", By.cssSelector("div.stm-tst-Yes"));
+	private static final By2 yesButtonInConfirmMonitorDeletion = new By2("'Yes' button in confirmation dialog for deleting the monitor", By.cssSelector("div.stm-tst-Yes"));
 
 
 	// MONITORS link in upper menu in ASSETS tab
-	private static final By2 monitorsLinkInUpperTab = new By2("Clicking the monitors link in upper menu", By.cssSelector("div.stm-tst-monitor-tab"));
+	private static final By2 monitorsLinkInUpperTab = new By2("'monitors' link in upper menu", By.cssSelector("div.stm-tst-monitor-tab"));
 
 	// "New Monitor" title of a new Load Generator ==>  $x("//a[@title='New Test']")
 	//private static final By2 newMonitorTitle = new By2("The new Monitor title", By.cssSelector(MainConfig.StormRunnerSitescopeServerName));
